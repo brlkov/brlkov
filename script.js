7205cb5fc09adedab80d9687fd3c81213a34730b
brlkov <sergeyburlakovspb@gmail.com>
2023-03-21 15:48:55 +0300

    3

--- script.js
+++ script.js
@@ -59,6 +59,7 @@ document.getElementById("next").addEventListener("click", () =>
         document.getElementById("pr3").style.display = "none";
         document.getElementById("pr4").style.display = "none";
         document.getElementById("pr5").style.display = "none";
+        document.getElementById("pr6").style.display = "none";
         counter = 2;
     }
     else if(counter === 2) {
@@ -67,6 +68,7 @@ document.getElementById("next").addEventListener("click", () =>
         document.getElementById("pr3").style.display = "none";
         document.getElementById("pr4").style.display = "none";
         document.getElementById("pr5").style.display = "none";
+        document.getElementById("pr6").style.display = "none";
         counter = 3;
     }
     else if(counter === 3) {
@@ -75,6 +77,7 @@ document.getElementById("next").addEventListener("click", () =>
         document.getElementById("pr3").style.display = "block";
         document.getElementById("pr4").style.display = "none";
         document.getElementById("pr5").style.display = "none";
+        document.getElementById("pr6").style.display = "none";
         counter = 4;
     }
     else if(counter === 4) {
@@ -83,6 +86,7 @@ document.getElementById("next").addEventListener("click", () =>
         document.getElementById("pr3").style.display = "none";
         document.getElementById("pr4").style.display = "block";
         document.getElementById("pr5").style.display = "none";
+        document.getElementById("pr6").style.display = "none";
         counter = 5;
     }
     else if(counter === 5) {
@@ -91,6 +95,16 @@ document.getElementById("next").addEventListener("click", () =>
         document.getElementById("pr3").style.display = "none";
         document.getElementById("pr4").style.display = "none";
         document.getElementById("pr5").style.display = "block";
+        document.getElementById("pr6").style.display = "none";
+        counter = 6;
+    }
+    else if(counter === 6) {
+        document.getElementById("pr1").style.display = "none";
+        document.getElementById("pr2").style.display = "none";
+        document.getElementById("pr3").style.display = "none";
+        document.getElementById("pr4").style.display = "none";
+        document.getElementById("pr5").style.display = "none";
+        document.getElementById("pr6").style.display = "block";
         counter = 1;
     }
 });
